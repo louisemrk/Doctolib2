1d621ea9474a31bc749be71d6c84cf6c945de245
CHarles Raffegeau <charles.raffegeau@student#hyhgit remote set-url origin git@github.com:raffegeau/CSTwitterAnalysis.gitgit config --global user.email charles.raffegeau@student>
2018-11-19 13:36:08 +0100

    réarrangement fichiers+ correction fichiers json

---
+++ Docto_MVP1/MVP1/lecture_candidats.py
@@ -0,0 +1,22 @@
+import pandas
+import json
+
+
+def dataframe_exploitable(filename_clean,clefs_utiles):
+    '''ce programme importe des donnees sous forme de fichier json qui sont à l'adresse filename_clean et renvoie un dataframe
+    qui a pour colonnes des clefs utiles du tweet'''
+    with open(filename_clean) as f:
+        data = json.load(f)
+        dictionnaire_utile=[]
+        for candidat in data["Candidats"]:
+            caract_nouveau_candidat={}
+            for k in clefs_utiles:
+                if k != "len":
+                    caract_nouveau_candidat[k]=candidat[k]
+            dictionnaire_utile.append(caract_nouveau_candidat)
+        donnees_exploitables=pandas.DataFrame(dictionnaire_utile)
+        return donnees_exploitables
+
+
+data = dataframe_exploitable("candidats.json",["first_name","last_name"])
+

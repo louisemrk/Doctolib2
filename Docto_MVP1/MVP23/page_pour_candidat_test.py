5b8cb8836c57eef79c2e854605764ba6d4d6eb71
CHarles Raffegeau <charles.raffegeau@student#hyhgit remote set-url origin git@github.com:raffegeau/CSTwitterAnalysis.gitgit config --global user.email charles.raffegeau@student>
2018-11-23 14:01:40 +0100

    test

---
+++ Docto_MVP1/MVP23/page_pour_candidat_test.py
@@ -0,0 +1,15 @@
+import pytest
+from MVP23.Page_pour_candidat import *
+
+def test_code_html():
+    assert type(code_html())==str
+
+def test_code_html():
+    assert type(html_candidat("a1"))==str
+
+
+
+
+
+
+
